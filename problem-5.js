a04c0dbd57c2fc388116f1d5fd9ca915f72ec0cf
Ayan <sujon.seo.bd@gmail.com>
2025-08-14 18:56:13 +0600

    Problem-5 Done

--- problem-5.js
+++ problem-5.js
@@ -1,9 +1,42 @@
 // Problem-05: Exam Result Report Generator
 function  resultReport( marks ) {
           // You have to write your code here
-    
 
-
-
-    
+if(!Array.isArray(marks)){
+    return "Invalid"; //Checking Validation
 }
+
+let pass = 0;
+let fail = 0; 
+let totalSum = 0;
+
+for(let i = 0; i < marks.length; i++){
+    if(marks[i] >= 40){
+        pass++;
+    }
+    else{
+        fail++;
+    }
+}
+for(let i = 0; i < marks.length; i++){
+   totalSum += marks[i];
+}
+let average= totalSum / marks.length;
+
+//Checking empty array and fixed (NaN)
+if(marks.length === 0){
+     average = 0; 
+}
+else{
+    average = totalSum / marks.length;
+}
+
+let finalScore = 'finalScore: ' + Math.round(average) + ', pass: ' + pass + ', fail: ' + fail;
+return finalScore;
+
+}
+
+let output = resultReport(
+[98, 87, 67, 91, 92, 33, 87]
+);
+console.log(output);
--- solutions.js
+++ solutions.js
@@ -69,15 +69,40 @@ function  isSame(arr1 , arr2 ) {
 
 // =====================================================
 
+// Problem-05: Exam Result Report Generator
+function  resultReport( marks ) {
+          // You have to write your code here
 
+if(!Array.isArray(marks)){
+    return "Invalid"; //Checking Validation
+}
 
+let pass = 0;
+let fail = 0; 
+let totalSum = 0;
 
+for(let i = 0; i < marks.length; i++){
+    if(marks[i] >= 40){
+        pass++;
+    }
+    else{
+        fail++;
+    }
+}
+for(let i = 0; i < marks.length; i++){
+   totalSum += marks[i];
+}
+let average= totalSum / marks.length;
 
+//Checking empty array and fixed (NaN)
+if(marks.length === 0){
+     average = 0; 
+}
+else{
+    average = totalSum / marks.length;
+}
 
+let finalScore = 'finalScore: ' + Math.round(average) + ', pass: ' + pass + ', fail: ' + fail;
+return finalScore;
 
-
-
-
-
-
-// =====================================================
+}
