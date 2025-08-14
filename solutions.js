2fc4d889cf7986540c20059cc0c3432bb9989f58
Ayan <sujon.seo.bd@gmail.com>
2025-08-14 09:52:23 +0600

    Problem-3 done

--- solutions.js
+++ solutions.js
@@ -24,8 +24,26 @@ return cleanAndCapitalize;
 }
 
 // =====================================================
+// Problem-03 : FIFA Best Team Award 
+function  bestTeam( player1, player2 ) {
+          // You have to write your code here
+          if( typeof player1 !== "object" || typeof player2 !== "object" ){
+            return "Invalid";
+          }
+          let countPlayer1 = player1.foul + player1.cardY + player1.cardR;
+          let conuntPlayer2 = player2.foul + player2.cardY + player2.cardR;
 
-
+          if(countPlayer1 < conuntPlayer2){
+            return player1.name;
+          }
+          else if(countPlayer1 == conuntPlayer2){
+            return "Tie";
+          }
+          else{
+            return player2.name;
+          }
+}
+// =====================================================
 
 
 
